13113e0870731ba2f44bbfbcc301f65eb87425cb
banditajenaGT <banditajena2001@gmail.com>
2024-03-15 11:34:13 +0530

    loader fixed

--- src/components/pages/Loader.jsx
+++ src/components/pages/Loader.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const Loader = () => {
   return (
     <Html>
-      <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-black-100 border-t-black-500 rounded-full animate-spin" />
+      <div  className="absolute top-[-4rem] left-[-1.8rem]">
+        <div className="w-20 h-20 border-2 border-opacity-50 border-black-100 border-t-black-500 rounded-full animate-spin" />
       </div>
     </Html>
   );
